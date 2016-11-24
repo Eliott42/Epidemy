fa6436f4e6d1d6ab3161b1016cb51fbad4356689
eliott Jouhaud <eliott@kashyyyk.local>
2016-11-24 10:54:20 +0100

    Initial Commit

---
+++ Epidemy/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Epidemy
+//
+//  Created by Eliott Jouhaud on 24/11/2016.
+//  Copyright © 2016 Eliott Jouhaud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
